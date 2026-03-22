19f6aae376bc3e361a5386185da13199d5712901
alma <a@gamil.com>
2026-03-22 23:04:47 +0100

    feat: base routeing

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,15 @@
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $baseUrl = '/vector/public';
 $url = str_replace($baseUrl, '', $url);
+$method = $_SERVER['REQUEST_METHOD'];
 echo "url: ".$url." <br>";
-if ($url==='/api/events') {
+if ($url==='/api/events') {//  && $method === 'GET'
     echo "api: get events";
+    return;
 }
+if (preg_match('#^/api/events/(\d+)/register$#', $url, $matches)) {// && $method === 'POST'
+    var_dump($matches);
+    return;
+}
+echo "not found";
 ?>
