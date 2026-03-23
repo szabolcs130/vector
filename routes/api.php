0f72602de2a7750b4a2e1f66b4740f96fcc0eeb3
alma <a@gamil.com>
2026-03-23 08:57:26 +0100

    feat: implement event listening endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
+require_once __DIR__ . '/../core/Database.php';
+require_once __DIR__ . '/../app/repositories/EventRepository.php';
+require_once __DIR__ . '/../app/services/EventService.php';
+require_once __DIR__ . '/../app/controllers/EventController.php';
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $baseUrl = '/vector/public';
 $url = str_replace($baseUrl, '', $url);
 $method = $_SERVER['REQUEST_METHOD'];
-echo "url: ".$url." <br>";
-if ($url==='/api/events') {//  && $method === 'GET'
-    echo "api: get events <br>";
-    /*$db=Database::getConnection();
-    $stmt=$db->query("SELECT * FROM events");
-    var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));*/
+if ($url==='/api/events' && $method === 'GET') {
+    $controller=new EventController(new EventService(new EventRepository));
+    $controller->getEventsFreeSpot();
     return;
 }
 if (preg_match('#^/api/events/(\d+)/register$#', $url, $matches)) {// && $method === 'POST'
